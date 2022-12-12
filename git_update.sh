eead3029661f707b0cdcfd5af3edb3b777bef925
Mr.Ethics <vanishfate@gmail.com>
2022-12-12 18:06:58 +0800

    更新git多个文件功能

--- git_update.sh
+++ git_update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-read -t 30 -e -p "Please input file name or file list: " filenames;
+read -t 30 -e -p "Please input file name or file list: " -a filenames;
 
 read -t 30 -e -p "Please input infomation for update: " information;
 
-if [ -z $filenames -o -z $information ]
+if [[ -z $filenames || -z $information ]]
 then
 	echo "file name or information can't be empty,please run again"
 else
-	git add $filenames
+	git add ${filenames[@]}
 	git commit -m $information
 	git push origin master
 fi
