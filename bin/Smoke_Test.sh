864f732f39ea5655503fd752d76785228164a389
Mr.Ethics <vanishfate@gmail.com>
2022-12-26 14:15:04 +0800

    完善Smoke

--- bin/Smoke_Test.sh
+++ bin/Smoke_Test.sh
@@ -4,8 +4,9 @@ adb get-state;
 
 if [[ $? -ne 0 ]]; then
 	echo "Please connect devices first"
+	exit
 else
-	Clientbase=`adb shell getprop ro.com.google.clientbase`
+	Clientidbase=`adb shell getprop ro.com.google.clientidbase`
 	TierFlag=`adb shell pm list features | grep "com.google.android.tv"`
 	AssistTriggering=`adb shell pm list features | grep 'GLOBAL_ASSIST_TRIGGERING'`
 	Serial_Number=`adb shell getprop ro.boot.serialno`
@@ -13,9 +14,15 @@ else
 	IPV6=`adb shell ip -6 addr ls wlan0 | grep inet6 | awk '{printf $2}'`
 	OEMKEY=`adb shell getprop ro.oem.key1`
 	AndroidID=`adb shell settings get secure android_id`
-	echo "The result is as follows"
-	echo <<EOF
-	HELLO WORLD
-	YES
-	EOF
 fi
+
+cat >/tmp/smoke.txt <<EOF
+Clientidbase: $Clientidbase
+TierFlag: $TierFlag
+AssisTriggering: $AssistTriggering
+SerialNumber: $Serial_Number
+MAC: $MAC
+IPV6: $IPV6
+OEMKEY: $OEMKEY
+AndroidID: $AndroidID
+EOF
